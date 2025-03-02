26cb81faf0939b21e8123999ce0058e2d59e3a11
emirrcaglar <emirrcaglar@gmail.com>
2025-03-02 19:52:56 +0300

    ch12

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 from config import Config
 
 app = Flask(__name__)
@@ -15,8 +16,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-mail = Mail(app)
 login.login_view = 'login'
+mail = Mail(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
